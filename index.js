889be2f886712212d862007b6d97cea8b40b9fa5
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 12:36:45 -0300

    refact: adding .js ext

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from "express";
 import dotenv from "dotenv"
 import cors from 'cors';
 
-import authStudentRouter   from "./api/routes/auth.student.routes"
+import authStudentRouter   from "./api/routes/auth.student.routes.js"
 import authProfessorRouter from "./api/routes/auth.professor.routes.js"
-import studentRouter       from "./api/routes/student.routes"
-import professorRouter     from "./api/routes/professor.routes"
+import studentRouter       from "./api/routes/student.routes.js"
+import professorRouter     from "./api/routes/professor.routes.js"
 import postRouter from "./api/routes/disciplines.routes.js"
 import connectDatabase from "./api/database/manageDB.js"
 

615df3ad32f5d4bc4c41782857d7a05730bb69c0
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 10:33:35 -0300

    rename files

---
+++ api/controllers/auth.professor.controller.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt';
+import { professorLoginService, generateToken} from '../services/auth.service.js';
+
+const login = async (req, res) => {
+    try {
+        const { matricula, password } = req.body;
+        const user = await professorLoginService(matricula);
+            
+         if (!user) {
+            return res.status(400).send({ message: "Matricula or Password not found" });
+         }
+        const passwordIsValid = await bcrypt.compare(password, user.password);
+         if (!passwordIsValid ) {
+            return res.status(400).send({ message: "Matricula or Password not found" });
+         }
+
+         const token = generateToken(user.id);
+
+        res.send({token:token});
+    } catch (error) {
+      res.status(500).send({ 'message': error.message })
+    }
+};
+
+export { login };

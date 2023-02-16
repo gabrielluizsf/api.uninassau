b4436bdd1b771839e400c84d2efee4985cceff78
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 09:44:22 -0300

    Student service implemented

---
+++ api/services/student.service.js
@@ -0,0 +1,20 @@
+import Student from '../database/models/Students';
+
+export const createService   = (body)=> Student.create(body);
+export const findAllService  = ()=> Student.find();
+export const findByIDService = (id)=> Student.findById(id);
+export const updateService   = (
+id,
+name,
+matricula,
+password,
+hourEnd,
+hourStart,
+useVoiceChannel,
+weekDays,
+yearsProgramming
+)=> Student.findOneAndUpdate(
+    { _id: id }, 
+    {name,username,email,password,avatar,background}
+);
+export const deleteService = (id)=>Student.findOneAndDelete({_id: id});

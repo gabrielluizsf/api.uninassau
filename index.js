a072d1d981ea14f9a540066d38fb34ff3a9ecf07
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 12:01:21 -0300

    index created

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv"
+import cors from 'cors';
+
+import authStudentRouter   from "./api/routes/auth.student.routes"
+import authProfessorRouter from "./api/routes/auth.professor.routes.js"
+import studentRouter       from "./api/routes/student.routes"
+import professorRouter     from "./api/routes/professor.routes"
+import postRouter from "./api/routes/disciplines.routes.js"
+import connectDatabase from "./api/database/manageDB.js"
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3700;
+const allowedOrigin = process.env.WEBSITE_URL;
+
+connectDatabase();
+app.use(cors({
+    origin: [`${allowedOrigin}`],
+    methods: ["GET", "POST","PATCH","DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.use(express.json());
+
+app.use("/students",studentRouter);
+app.use("/professors",professorRouter);
+app.use("/login/student",authStudentRouter);
+app.use("/login/professor",authProfessorRouter);
+app.use("/disciplines",postRouter);
+
+
+app.listen(port,()=>console.log(`[Server Online]: {
+                                               PORT:${port}
+                                                  }
+                              `));

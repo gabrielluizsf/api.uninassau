9be73853c5435be91fd3fac64389d30997f09143
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 11:22:26 -0300

    global middleware created

---
+++ api/middlewares/global.middlewares.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import { findByIdService } from "../services/discipline.service.js";
+import professorService from "../services/professor.service.js";
+import studentService from "../services/student.service.js";
+
+const isValidId = (req,res,next)=>{
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(400).send({message:"Invalid ID"});
+    }
+    next();
+}
+const isValidProfessor = async (req,res,next)=>{
+    const id = req.params.id;
+    const professor = await professorService.findByIdService(id);
+    if(!professor){
+      return res.status(400).send({message:"[ERROR] PROFESSOR NOT FOUND"});
+   }
+   req.id = id;
+   req.professor = professor;
+   next();
+}
+const isValidStudent = async (req,res,next)=>{
+    const id = req.params.id;
+    const student = await studentService.findByIdService(id);
+    if(!student){
+      return res.status(400).send({message:"[ERROR] STUDENT NOT FOUND"});
+   }
+   req.id = id;
+   req.student = student;
+   next();
+}
+export const isValidDiscipline = async (req,res,next)=>{
+   const id = req.params.id;
+   const discipline = await findByIdService(id);
+   if(!discipline){
+    return res.status(400).send({message:"[ERROR] DISCIPLINE NOT FOUND"});
+   }
+   
+  if(discipline.user._id != req.userId){
+    return res.send({message: "You didn't update this discipline"});
+  }
+   const {title, banner} = req.body;
+   if (!title && !banner){
+    res.status(400).send({message:"Submit at least one field to update the discipline"});
+   }
+   const titleContent  = title;
+   const bannerContent = banner;
+  req.id      = id;
+  req.title   = titleContent;
+  req.banner  = bannerContent;
+  next();
+}
+
+export default{isValidId,isValidProfessor, isValidStudent}

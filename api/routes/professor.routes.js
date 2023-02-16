58452c8c87d7706e8fb3fa960ddd29b01fc665af
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 11:51:40 -0300

    DELETE IMPORTS

--- api/routes/professor.routes.js
+++ api/routes/professor.routes.js
@@ -1,6 +1,4 @@
 import  {  Router } from "express";
-import globalMiddleware from "../middlewares/global.middlewares.js";
-import {authMiddleware} from "../middlewares/auth.professor.middleware.js";
 import userController from "../controllers/professor.controller.js";
 
 const router = Router();
